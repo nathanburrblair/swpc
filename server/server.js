7c8dfacf6966d54dd50210135959f0d801b6af03
Nathan Blair <nathanburrblair@gmail.com>
2019-02-13 20:20:56 -0700

    overhaul

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 app.use( express.static( `${__dirname}/../build` ) );
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 const port = 4850
 
 app.listen(port, () => {
-    console.log(`The magic be happening on port ${port}`)
+  console.log(`The magic be happening on port ${port}`)
 })
--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
+import logo from "./logo.svg";
 import "./App.css";
 import Routes from "./Routes";
 import { HashRouter } from "react-router-dom";
-// import ScrollToTop from "../src/components/ScrollToTop";
-import SiteHeader from "../src/components/SiteHeader/SiteHeader";
-import Footer from "../src/components/Footer/Footer";
 
 class App extends Component {
   render() {
     return (
       <HashRouter>
-        {/* <ScrollToTop> */}
-          <div className="App">
-            <SiteHeader />
-            {Routes}
-            <Footer />
-          </div>
-        {/* </ScrollToTop> */}
+        <div className="App">{Routes}</div>
       </HashRouter>
     );
   }
